22c9dbaaba09aea31c6e686165524d361c2c137b
Turtleswin <lindseylovr@yahoo.com>
2013-03-01 12:40:25 -0700

    Added Scholomance SQL file.

---
+++ Dungeons/Scholomance.sql
@@ -0,0 +1,81 @@
+-- NOT YEET COMPLETE!
+
+UPDATE `creature_template` SET minlevel=68 WHERE entry=10489;
+UPDATE `creature_template` SET maxlevel=70 WHERE entry=10489;
+UPDATE `creature_template` SET health_mod=12 WHERE entry=10489;
+UPDATE `creature_template` SET mindmg=849 WHERE entry=10489;
+UPDATE `creature_template` SET maxdmg=1029 WHERE entry=10489;
+UPDATE `creature_template` SET dmg_multiplier=1.4 WHERE entry=10489;
+UPDATE `creature_template` SET minlevel=69 WHERE entry=10470;
+UPDATE `creature_template` SET maxlevel=71 WHERE entry=10470;
+UPDATE `creature_template` SET mindmg=849 WHERE entry=10470;
+UPDATE `creature_template` SET maxdmg=1029 WHERE entry=10470;
+UPDATE `creature_template` SET dmg_multiplier=1 WHERE entry=10470;
+UPDATE `creature_template` SET minlevel=69 WHERE entry=10499;
+UPDATE `creature_template` SET maxlevel=71 WHERE entry=10499;
+UPDATE `creature_template` SET mindmg=849 WHERE entry=10499;
+UPDATE `creature_template` SET maxdmg=1029 WHERE entry=10499;
+UPDATE `creature_template` SET dmg_multiplier=1 WHERE entry=10499;
+UPDATE `creature_template` SET minlevel=70 WHERE entry=10471;
+UPDATE `creature_template` SET maxlevel=71 WHERE entry=10471;
+UPDATE `creature_template` SET mindmg=849 WHERE entry=10471;
+UPDATE `creature_template` SET maxdmg=1029 WHERE entry=10471;
+UPDATE `creature_template` SET dmg_multiplier=1 WHERE entry=10471;
+UPDATE `creature_template` SET minlevel=70 WHERE entry=11551;
+UPDATE `creature_template` SET maxlevel=71 WHERE entry=11551;
+UPDATE `creature_template` SET mindmg=849 WHERE entry=11551;
+UPDATE `creature_template` SET maxdmg=1029 WHERE entry=11551;
+UPDATE `creature_template` SET dmg_multiplier=1 WHERE entry=11551;
+UPDATE `creature_template` SET minlevel=70 WHERE entry=11582;
+UPDATE `creature_template` SET maxlevel=71 WHERE entry=11582;
+UPDATE `creature_template` SET mindmg=849 WHERE entry=11582;
+UPDATE `creature_template` SET maxdmg=1029 WHERE entry=11582;
+UPDATE `creature_template` SET dmg_multiplier=1 WHERE entry=11582;
+UPDATE `creature_template` SET minlevel=70 WHERE entry=10476;
+UPDATE `creature_template` SET maxlevel=70 WHERE entry=10476;
+UPDATE `creature_template` SET mindmg=849 WHERE entry=10476;
+UPDATE `creature_template` SET maxdmg=1029 WHERE entry=10476;
+UPDATE `creature_template` SET dmg_multiplier=1 WHERE entry=10476;
+UPDATE `creature_template` SET minlevel=70 WHERE entry=11258;
+UPDATE `creature_template` SET maxlevel=71 WHERE entry=11258;
+UPDATE `creature_template` SET mindmg=849 WHERE entry=11258;
+UPDATE `creature_template` SET maxdmg=1029 WHERE entry=11258;
+UPDATE `creature_template` SET dmg_multiplier=1 WHERE entry=11258;
+UPDATE `creature_template` SET minlevel=70 WHERE entry=10487;
+UPDATE `creature_template` SET maxlevel=71 WHERE entry=10487;
+UPDATE `creature_template` SET mindmg=849 WHERE entry=10487;
+UPDATE `creature_template` SET maxdmg=1029 WHERE entry=10487;
+UPDATE `creature_template` SET dmg_multiplier=1 WHERE entry=10487;
+UPDATE `creature_template` SET minlevel=70 WHERE entry=10469;
+UPDATE `creature_template` SET maxlevel=71 WHERE entry=10469;
+UPDATE `creature_template` SET mindmg=849 WHERE entry=10469;
+UPDATE `creature_template` SET maxdmg=1029 WHERE entry=10469;
+UPDATE `creature_template` SET dmg_multiplier=1 WHERE entry=10469;
+UPDATE `creature_template` SET minlevel=70 WHERE entry=10498;
+UPDATE `creature_template` SET maxlevel=71 WHERE entry=10498;
+UPDATE `creature_template` SET mindmg=849 WHERE entry=10498;
+UPDATE `creature_template` SET maxdmg=1029 WHERE entry=10498;
+UPDATE `creature_template` SET dmg_multiplier=1 WHERE entry=10498;
+UPDATE `creature_template` SET minlevel=70 WHERE entry=10477;
+UPDATE `creature_template` SET maxlevel=71 WHERE entry=10477;
+UPDATE `creature_template` SET mindmg=849 WHERE entry=10477;
+UPDATE `creature_template` SET maxdmg=1029 WHERE entry=10477;
+UPDATE `creature_template` SET dmg_multiplier=1 WHERE entry=10477;
+UPDATE `creature_template` SET minlevel=72 WHERE entry=11662;
+UPDATE `creature_template` SET maxlevel=72 WHERE entry=11662;
+UPDATE `creature_template` SET mindmg=2019 WHERE entry=11662;
+UPDATE `creature_template` SET maxdmg=3029 WHERE entry=11662;
+UPDATE `creature_template` SET dmg_multiplier=2 WHERE entry=11662;
+UPDATE `creature_template` SET health_mod=40 WHERE entry=11662;
+UPDATE `creature_template` SET minlevel=72 WHERE entry=10503;
+UPDATE `creature_template` SET maxlevel=72 WHERE entry=10503;
+UPDATE `creature_template` SET mindmg=2019 WHERE entry=10503;
+UPDATE `creature_template` SET maxdmg=3029 WHERE entry=10503;
+UPDATE `creature_template` SET dmg_multiplier=2 WHERE entry=10503;
+UPDATE `creature_template` SET health_mod=40 WHERE entry=10503;
+
+
+
+
+
+-- Extra entries: 11551, 11582, 10476, 11258, 10487, 10477, 10469, 10498, 10477, 11662--
